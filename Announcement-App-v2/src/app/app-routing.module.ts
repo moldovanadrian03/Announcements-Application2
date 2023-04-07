1c136e9ae28951bbb36517b48e9a7fe45525e4a4
Adrian <moldovan.adrian.alexandru03@gmail.com>
2023-04-07 17:19:32 +0300

    Generated main-layout component, set it as main-route and nested announcements-list component inside.

--- Announcement-App-v2/src/app/app-routing.module.ts
+++ Announcement-App-v2/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AnnouncementsListComponent } from './pages/announcements-list/announcements-list.component';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
 
 const routes: Routes = [
-  { path: '', component: AnnouncementsListComponent }
+  { path: '', component: MainLayoutComponent, children: [
+    { path: '', component: AnnouncementsListComponent }
+  ] }
 ];
 
 @NgModule({
--- Announcement-App-v2/src/app/app.module.ts
+++ Announcement-App-v2/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AnnouncementsListComponent } from './pages/announcements-list/announcements-list.component';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AnnouncementsListComponent
+    AnnouncementsListComponent,
+    MainLayoutComponent
   ],
   imports: [
     BrowserModule,
