52a42dde7f43e191ed9ecb53bf3a1d0001373495
Adrian <moldovan.adrian.alexandru03@gmail.com>
2023-04-07 17:10:29 +0300

    Generated the AnnouncemnetList component and routed it.

---
+++ Announcement-App-v2/src/app/pages/announcements-list/announcements-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-announcements-list',
+  templateUrl: './announcements-list.component.html',
+  styleUrls: ['./announcements-list.component.scss']
+})
+export class AnnouncementsListComponent {
+
+}
