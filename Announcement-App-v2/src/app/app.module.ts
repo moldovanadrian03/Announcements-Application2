8ba01190febc058b6502ca1fc34b9a33f9b961e6
Adrian <moldovan.adrian.alexandru03@gmail.com>
2023-04-07 18:15:38 +0300

    Added styling for the top-bar.

--- Announcement-App-v2/src/app/app.module.ts
+++ Announcement-App-v2/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AnnouncementsListComponent } from './pages/announcements-list/announcements-list.component';
 import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
